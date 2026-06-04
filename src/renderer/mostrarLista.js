212963a1d4ae8b42f2b7592f507d7b300706790e
Alejandro-Elias <alejandroElias25@gmail.com>
2026-06-04 00:30:33 -0300

    repara ultimo tema oculto en playlist

--- src/renderer/mostrarLista.js
+++ src/renderer/mostrarLista.js
@@ -6,11 +6,11 @@ export const mostrarLista = () => {
   listaReproduccion.innerHTML = " ";
 
   if (playList.length > 0) {
-    for (let i = 0; i < playList.length - 1; i++) {
+    for (let i = 0; i < playList.length; i++) {
       listaReproduccion.innerHTML += `<li class="track" >${i + 1} ${playList[i].archivo}</li>`;
     }
   } else {
-    for (let i = 0; i < lista.length - 1; i++) {
+    for (let i = 0; i < lista.length; i++) {
       listaReproduccion.innerHTML += `<li class="track" >${i + 1} ${lista[i].archivo}</li>`;
     }
   }
