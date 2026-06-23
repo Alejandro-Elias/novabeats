de70a3b8d02f8145a286629aa24de6ff212ae218
Alejandro-Elias <alejandroElias25@gmail.com>
2026-06-23 15:48:29 -0300

    agrega enter para guardar playlist

--- src/renderer/guardarPlaylist.js
+++ src/renderer/guardarPlaylist.js
@@ -2,37 +2,47 @@ import { getStorage, setStorage } from "../modules/localStorage.js";
 import { playList } from "./buttons/play/loadList.js";
 import { mostrarlistas } from "./mostrarLista.js";
 
+const guardarPlayList = () => {
+  const guardarPlaylist = document.getElementById("guardarPlaylist");
+
+  const listaActual = playList;
+
+  let listaAguardar = getStorage("playlists") || [];
+
+  if (listaActual.length > 0) {
+    const nombreLista =
+      inputGuardar.value ||
+      `playList ${listaAguardar > 0 ? listaAguardar[listaAguardar.length - 1].id + 1 : 1}`;
+
+    listaAguardar.push({
+      id:
+        listaAguardar.length > 0
+          ? listaAguardar[listaAguardar.length - 1].id + 1
+          : 1,
+      name: nombreLista,
+      playList: playList,
+    });
+
+    setStorage("playlists", listaAguardar);
+
+    inputGuardar.value = ``;
+
+    mostrarlistas();
+  } else {
+    alert("No se puede guardar una lista vacia");
+  }
+};
+
 export const guardarLista = () => {
   const inputGuardar = document.getElementById("inputGuardar");
   const listBtn = document.getElementById("listBtn");
   listBtn.addEventListener("click", () => {
-    const guardarPlaylist = document.getElementById("guardarPlaylist");
+    guardarPlayList()
+  });
 
-    const listaActual = playList;
-
-    let listaAguardar = getStorage("playlists") || [];
-
-    if (listaActual.length > 0) {
-      const nombreLista =
-        inputGuardar.value ||
-        `playList ${listaAguardar > 0 ? listaAguardar[listaAguardar.length - 1].id + 1 : 1}`;
-
-      listaAguardar.push({
-        id:
-          listaAguardar.length > 0
-            ? listaAguardar[listaAguardar.length - 1].id + 1
-            : 1,
-        name: nombreLista,
-        playList: playList,
-      });
-
-      setStorage("playlists", listaAguardar);
-
-      inputGuardar.value = ``;
-
-      mostrarlistas();
-    } else {
-      alert("No se puede guardar una lista vacia");
+  inputGuardar.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      guardarPlayList();
     }
   });
 };
